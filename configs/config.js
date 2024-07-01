9a985dd90de6e30525e1f5f597af682730849764
Preksha bhalani <59721431+prekshabhalani@users.noreply.github.com>
2024-07-01 16:15:29 +0530

    🏗️ Made architectural changes
    Update config file as per requirement only need one environment structure so no need for custom env

--- configs/config.js
+++ configs/config.js
@@ -1,14 +1,12 @@
 /****************************
  Configuration
  ****************************/
-// For environment variables [will work with .env file]
-// require('custom-env').env("dev")
-require('custom-env').env("dev")
+require('dotenv').config();
 
-const ENV_VARIABLES = process.env;
-if (!process.env.SOME_REQUIRED_VARIABLE) {
-    console.warn(`Warning: No environment file found for environment . Please ensure the '.env.' file is present.`);
-}
+
 module.exports = {
-    ...ENV_VARIABLES,
+
+    SERVER: {
+        port: process.env.SERVER_PORT
+    }
 };
