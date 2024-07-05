04492dc58963d958ef4a4af0333c6369abdfa8d8
Preksha bhalani <59721431+prekshabhalani@users.noreply.github.com>
2024-07-05 22:05:09 +0530

    👔 Add search by name and phoneNumber functionality for user

---
+++ app/modules/search/Validator.js
@@ -0,0 +1,33 @@
+const { body, validationResult, query } = require('express-validator');
+
+/********************************************************
+       @Method POST 
+       @Route '/search_by_name'
+   ********************************************************/
+const searchNumberValidation = [
+  query('phoneNumber').matches(/^\+?\d+$/).withMessage('Valid number is required'),
+];
+
+/********************************************************
+       @Method POST 
+       @Route '/search_by_phone_number'
+   ********************************************************/
+const searchNameValidation = [
+  query('searchQuery').isString().withMessage('Valid name is required'),
+];
+
+/**** express validate fields */
+const validate = (validations) => {
+  return async (req, res, next) => {
+    await Promise.all(validations.map(validation => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    res.status(400).json({ errors: errors.array() });
+  };
+};
+
+module.exports = { searchNumberValidation, searchNameValidation, validate };
