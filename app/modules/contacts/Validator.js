52c6766adc40f6ffdb61c19f727ec736e738fa05
Preksha bhalani <59721431+prekshabhalani@users.noreply.github.com>
2024-07-05 22:10:10 +0530

    🦺 Add validation feature

---
+++ app/modules/contacts/Validator.js
@@ -0,0 +1,26 @@
+const { body, validationResult, query } = require('express-validator');
+
+/********************************************************
+       @Method POST 
+       @Route '/personDetails'
+   ********************************************************/
+const personDetailsValidation = [
+  query('id').isUUID().withMessage('Valid id is required')
+];
+
+
+/**** express validate fields */
+const validate = (validations) => {
+  return async (req, res, next) => {
+    await Promise.all(validations.map(validation => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    res.status(400).json({ errors: errors.array() });
+  };
+};
+
+module.exports = { personDetailsValidation, validate };
